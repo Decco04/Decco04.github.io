83af51551f7b68ad8eeeeb71970342a8d575bd4b
Decco04 <malte.lindberg.e@skola.karlskrona.se>
2023-02-06 09:58:13 +0100

    hihihih

--- Homepage/homeAgain.js
+++ Homepage/homeAgain.js
@@ -1,14 +1,19 @@
     var vid = document.getElementById("myVideo");
     vid.playbackRate = 1; 
-    loadAnimation1();
+    myStartFunction();
+
+    function myStartFunction(){
+        var x = window.matchMedia("(max-width: 450px)")
+        if (x.matches) {
+            loadAnimation1();
 
     function loadAnimation1() {
         document.getElementById('bread-text-left').style.animation="bread-text-left-animation 1.5s cubic-bezier(.33,.17,.37,1)";
         window.setTimeout(loadAnimation2, 750);
     }
     function loadAnimation2() {
-        document.getElementById('bread-text-right').style.animation="bread-text-right-animation 1.5s cubic-bezier(.33,.17,.37,1)";
-        document.getElementById('bread-text-right').style.marginLeft="67%"
+        document.getElementById('bread-text-right').style.animation="bread-text-right-animation-small 1.8s cubic-bezier(.33,.17,.37,1)";
+        document.getElementById('bread-text-right').style.marginLeft="-62%";
         window.setTimeout(loadAnimation3, 1500);
         window.setTimeout(loadAnimation4, 1000);
     }
@@ -26,3 +31,44 @@
         document.getElementById('parent').style.animation="maps-animation 1.5s cubic-bezier(.33,.17,.37,1)";
         document.getElementById('parent').style.opacity="1";
     }
+         }
+        else {
+            loadAnimation1();
+
+    function loadAnimation1() {
+        document.getElementById('bread-text-left').style.animation="bread-text-left-animation 1.5s cubic-bezier(.33,.17,.37,1)";
+        window.setTimeout(loadAnimation2, 750);
+    }
+    function loadAnimation2() {
+        document.getElementById('bread-text-right').style.animation="bread-text-right-animation 1.5s cubic-bezier(.33,.17,.37,1)";
+        document.getElementById('bread-text-right').style.marginLeft="67%";
+        window.setTimeout(loadAnimation3, 1500);
+        window.setTimeout(loadAnimation4, 1000);
+    }
+    function loadAnimation3() {
+        document.getElementById('myVideo').style.animation="map-video-animation 1.5s cubic-bezier(.33,.17,.37,1)";
+        document.getElementById('myVideo').style.opacity="1";
+    }
+    function loadAnimation4() {
+        var elems = document.getElementsByClassName('grid-link'),
+        size = elems.length;
+        for (var i = 0; i < size; i++) {
+            var box = elems[i];
+            box.style.zIndex="1";
+        }
+        document.getElementById('parent').style.animation="maps-animation 1.5s cubic-bezier(.33,.17,.37,1)";
+        document.getElementById('parent').style.opacity="1";
+    }
+        }
+    }
+
+    function myFunction() {
+      var x = window.matchMedia("(max-width: 450px)")
+        if (x.matches) { // If media query matches
+        document.getElementById('bread-text-right').style.marginLeft="-62%";
+        } else {
+        document.getElementById('bread-text-right').style.marginLeft="67%";
+          
+        }
+      }      
+      window.addEventListener("resize", myFunction);
